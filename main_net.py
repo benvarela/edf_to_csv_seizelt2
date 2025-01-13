cd8fcb8fafd46c5a5c3cb3b510961b9640e9ca20
biomedepi <119166923+biomedepi@users.noreply.github.com>
2025-01-13 17:28:26 +0100

    Update main_net.py

--- main_net.py
+++ main_net.py
@@ -56,11 +56,13 @@ config.add_to_name = 'test'
 ###########################################
 ###########################################
 
-load_generators = True                                          # Boolean to load generators from file
-save_generators = False                                         # Boolean to save the training and validation generator objects. The training generator is saved with the dataset, frame and sample type properties in the name of the file. The validation generator is always using the sequential windowed method.
+load_generators = False                                          # Boolean to load generators from file
+save_generators = True                                         # Boolean to save the training and validation generator objects. The training generator is saved with the dataset, frame and sample type properties in the name of the file. The validation generator is always using the sequential windowed method.
 
 main_func.train(config, load_generators, save_generators)
 
+print('Getting predictions on the test set...')
 main_func.predict(config)
 
+print('Getting evaluation metrics...')
 main_func.evaluate(config)
