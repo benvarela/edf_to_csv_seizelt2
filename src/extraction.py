bf109da4a06303401400334a8d6d236ca88c1f42
benvarela <benvarela4@gmail.com>
2025-05-15 17:14:16 +1000

    Removed redundant files

--- src/extraction.py
+++ src/extraction.py
@@ -8,8 +8,28 @@ import gzip
 # Insert your path to the dataset here
 path_to_dataset = '../Dataset/'
 SUBJECTS = 129
-EEG_CHANNEL = 'CROSStop SD'
+EEG_CHANNEL_1 = 'BTEleft SD'
+EEG_CHANNEL_2 = 'BTEright SD'
 HZ = 256
+
+# Initialisation of function that extracts data from a single run, once the channel is confirmed and the data is extracted from the .edf
+def pull_channel_run(EEG_CHANNEL: str, d: Data, a: Annotation, sub_dict: dict, rec) -> dict:
+    # Format into a numpy array
+    i = d.channels.index(EEG_CHANNEL)
+    run_data = d.data[i]
+    run_time = np.arange(run_data.shape[0]) * (1/HZ)
+    run_sz = np.zeros(run_data.shape[0])
+    for times in a.events:
+        onset = times[0]
+        offset = times[1]
+        istart = np.where(run_time == onset)[0][0]
+        iend = np.where(run_time == offset)[0][0]
+        run_sz[istart:iend] = 1
+
+    # Add to the sub_dict
+    sub_dict[rec[1]] = np.stack([run_data, run_time, run_sz])
+    sub_dict['run_list'].append(rec[1])
+    return sub_dict
 
 ## Build recordings list - variable recordings has the structure ['sub-001', 'run-01'], for all subjects and runs
 data_path = Path(path_to_dataset)
@@ -29,22 +49,10 @@ for sub in np.array(range(SUBJECTS)) + 1:
             a = Annotation.loadAnnotation(data_path.as_posix(), rec)
 
             # Using the desired EEG channel
-            if EEG_CHANNEL in d.channels:
-                # Format into a numpy array
-                i = d.channels.index(EEG_CHANNEL)
-                run_data = d.data[i]
-                run_time = np.arange(run_data.shape[0]) * (1/HZ)
-                run_sz = np.zeros(run_data.shape[0])
-                for times in a.events:
-                    onset = times[0]
-                    offset = times[1]
-                    istart = np.where(run_time == onset)[0][0]
-                    iend = np.where(run_time == offset)[0][0]
-                    run_sz[istart:iend] = 1
-
-                # Add to the sub_dict
-                sub_dict[rec[1]] = np.stack([run_data, run_time, run_sz])
-                sub_dict['run_list'].append(rec[1])
+            if EEG_CHANNEL_1 in d.channels:
+                sub_dict = pull_channel_run(EEG_CHANNEL_1, d, a, sub_dict, rec)
+            elif EEG_CHANNEL_2 in d.channels:
+                sub_dict = pull_channel_run(EEG_CHANNEL_2, d, a, sub_dict, rec)
 
     # Check whether any data was added, before attempting to export data. No data is added in the event none of the runs associated with a patient have desired EEG channel specified in EEG_CHANNEL
     if sub_dict['run_list']:
@@ -67,5 +75,3 @@ for sub in np.array(range(SUBJECTS)) + 1:
 ## Useful lines to copy and paste to interface with the classes
 #rec_data = Data.loadData(data_path.as_posix(), rec, modalities=['eeg'])
 #rec_annotations = Annotation.loadAnnotation(data_path.as_posix(), rec)
-
-
